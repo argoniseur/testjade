dc7936c4c2db751702ee9fa7f907b318a3ec4f76
felix <fel2fram@gmail.com>
2021-02-20 00:15:17 +0100

    ajout de tests pour le ping

---
+++ src/test/ConsumerMock3.java
@@ -0,0 +1,180 @@
+package test;
+
+import jade.core.Agent;
+import jade.core.AID;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
+public class ConsumerMock3 extends Agent {
+
+    private int ping = 0;
+    private AID connectedTo;
+    // list of producers
+    private AID[] producers;
+
+    // Put agent initializations here
+    protected void setup() {
+        // Printout a welcome message
+        System.out.println("Hallo! Buyer-agent "+getAID().getName()+" is ready.");
+
+        // Add a TickerBehaviour that schedules a request to seller agents every minute
+        addBehaviour(new TickerBehaviour(this, 10000) {
+            protected void onTick() {
+                System.out.println("Trying to buy energy");
+                // Update the list of seller agents
+                DFAgentDescription template = new DFAgentDescription();
+                ServiceDescription sd = new ServiceDescription();
+                sd.setType("energy-trading");
+                template.addServices(sd);
+                try {
+                    DFAgentDescription[] result = DFService.search(myAgent, template);
+                    System.out.println("Found the following seller agents:");
+                    producers = new AID[result.length];
+                    for (int i = 0; i < result.length; ++i) {
+                        producers[i] = result[i].getName();
+                        System.out.println(producers[i].getName());
+                    }
+                }
+                catch (FIPAException fe) {
+                    fe.printStackTrace();
+                }
+
+                // Perform the request
+                myAgent.addBehaviour(new RequestPerformer());
+            }
+        } );
+    }
+
+    // Put agent clean-up operations here
+    protected void takeDown() {
+        // Printout a dismissal message
+        System.out.println("Buyer-agent "+getAID().getName()+" terminating.");
+    }
+
+    /**
+     Inner class RequestPerformer.
+     This is the behaviour used by Book-buyer agents to request seller
+     agents the target book.
+     */
+    private class RequestPerformer extends Behaviour {
+        private AID bestSeller; // The agent who provides the best offer
+        private int bestUtility;  // The best offered price
+        private int repliesCnt = 0; // The counter of replies from seller agents
+        private MessageTemplate mt; // The template to receive replies
+        private int step = 0;
+
+        public void action() {
+            switch (step) {
+                case 0:
+                    // Send the cfp to all sellers
+                    ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+                    for (int i = 0; i < producers.length; ++i) {
+                        cfp.addReceiver(producers[i]);
+                    }
+                    cfp.setContent("Available ?");
+                    cfp.setConversationId("energy-trade");
+                    cfp.setReplyWith("cfp"+System.currentTimeMillis()); // Unique value
+                    myAgent.send(cfp);
+                    // Prepare the template to get proposals
+                    mt = MessageTemplate.and(MessageTemplate.MatchConversationId("energy-trade"),
+                            MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+                    step = 1;
+                    break;
+                case 1:
+                    // Receive all proposals/refusals from seller agents
+                    ACLMessage reply = myAgent.receive(mt);
+                    if (reply != null) {
+                        // Reply received
+                        if (reply.getPerformative() == ACLMessage.PROPOSE) {
+                            // This is an offer
+                            int utility;
+                            byte [] response = reply.getByteSequenceContent();
+
+                            if (response[0] == 0){
+                                utility = response[1];
+                            }else{
+                                utility = response[1] / 2;
+                            }
+                            if (bestSeller == null || utility < bestUtility) {
+                                // This is the best offer at present
+                                bestUtility = utility;
+                                bestSeller = reply.getSender();
+                            }
+                        }
+                        repliesCnt++;
+                        if (repliesCnt >= producers.length) {
+                            // We received all replies
+                            step = 5;
+                        }
+                    }
+                    else {
+                        block();
+                    }
+                    break;
+                case 2:
+                    // Send the purchase order to the seller that provided the best offer
+                    ACLMessage order = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+                    order.addReceiver(bestSeller);
+                    order.setContent("connect");
+                    order.setConversationId("energy-trade");
+                    order.setReplyWith("order"+System.currentTimeMillis());
+                    myAgent.send(order);
+                    // Prepare the template to get the purchase order reply
+                    mt = MessageTemplate.and(MessageTemplate.MatchConversationId("energy-trade"),
+                            MessageTemplate.MatchInReplyTo(order.getReplyWith()));
+                    step = 3;
+                    break;
+                case 3:
+                    // Receive the purchase order reply
+                    reply = myAgent.receive(mt);
+                    if (reply != null) {
+                        // Purchase order reply received
+                        if (reply.getPerformative() == ACLMessage.INFORM) {
+                            // Purchase successful. We can terminate
+                            System.out.println("Successfully connected to agent "+reply.getSender().getName());
+                            System.out.println("Utility = "+bestUtility);
+                            connectedTo = reply.getSender();
+                            step = 4;
+                            //myAgent.doDelete();
+                        }
+                        else {
+                            System.out.println("Attempt failed: Producer full.");
+                        }
+
+
+                    }
+                    else {
+                        block();
+                    }
+
+                    break;
+                case 4:
+                    block(20000);
+                    step = 5;
+
+                    break;
+                case 5:
+                    ping = 0;
+                    ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+                    msg.setContent("Ciao");
+                    msg.addReceiver(bestSeller);
+                    send(msg);
+                    this.myAgent.doDelete();
+                    step = 6;
+                    break;
+            }
+        }
+
+        public boolean done() {
+            if (step == 2 && bestSeller == null) {
+                System.out.println("Attempt failed: Producer not available");
+            }
+            return ((step == 2 && bestSeller == null) || step == 6);
+        }
+    }  // End of inner class RequestPerformer
+}
---
+++ src/test/LauncherMockp3.java
@@ -0,0 +1,30 @@
+package test;
+
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.StaleProxyException;
+
+public class LauncherMockp3 {
+    public static void main(String[] args) {
+        Runtime runtime = Runtime.instance();
+        Profile config = new ProfileImpl("localhost", 8888, null);
+        config.setParameter("gui", "true");
+        AgentContainer mc = runtime.createMainContainer(config);
+        AgentController ac, ap;
+
+        Object [] arg = new Object[2];
+        arg[0] = "1";
+        arg[1] = "30";
+
+        try {
+            ap = mc.createNewAgent("prod", ProducerMock3.class.getName(), arg);
+
+            ap.start();
+            ac = mc.createNewAgent("cons", ConsumerMock2.class.getName(), null);
+            ac.start();
+        } catch (StaleProxyException e) { }
+    }
+}
